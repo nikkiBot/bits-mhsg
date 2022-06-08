bfcf06c878ed5da44fcd34ef908793cef3a46602
Nishith Kumar <73156204+Nis0208@users.noreply.github.com>
2022-06-08 12:43:18 +0530

    carousel

--- src/components/CarouselContainer.jsx
+++ src/components/CarouselContainer.jsx
@@ -8,38 +8,87 @@ const CarouselContainer = () =>{
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
+            src="./carousel_images/Long_Long.png"
+            alt="logo"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
+            <h3>Our Headliner Events</h3>
+            <p>Checkout our events page for more details</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="./carousel_images/game-night.jpg"
+            alt="game night"
+          />
+          <Carousel.Caption>
+            <h3>Game Night</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
+            src="./carousel_images/game-night-1.jpg"
+            alt="game night 1"
           />
-      
           <Carousel.Caption>
-            <h3>Second slide label</h3>
+            <h3>Game Night</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
+            src="./carousel_images/silent-struggle.jpg"
+            alt="Silent Struggle"
           />
-      
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3>Silent Struggle</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </Carousel.Caption>
         </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="./carousel_images/silent-struggle-1.jpg"
+            alt="Silent Struggle"
+          />
+          <Carousel.Caption>
+            <h3>Silent Struggle</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="./carousel_images/talk-it-out.jpg"
+            alt="talk-it-out"
+          />
+          <Carousel.Caption>
+            <h3>Talk it Out</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="./carousel_images/talk-it-out-1.jpg"
+            alt="talk-it-out"
+          />
+          <Carousel.Caption>
+            <h3>Talk it Out</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
       </Carousel>
     )
 }
