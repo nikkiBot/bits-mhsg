e9927c184c49ac35ea72ac64f8446c0e966497e6
nikkiBot <f20200157@hyderabad.bits-pilani.ac.in>
2023-02-06 14:14:59 +0530

    -folder

---
+++ bits-mhsg/server/models/Post.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        postID :{
+            type : String,
+            unique : true,  
+            required: true,
+        },
+        postDescription: {
+            type: String,
+            required: true
+        },
+        postCover: {
+            type: String,
+            required: true 
+        },
+        postAuthor: {
+            type: String,
+            required: true
+        },
+        postCategories: {
+            type: Array,
+            required: false,
+        }
+    },
+    { timestamps: true} // This will automatically add the createdAt and the updatedAt field for us, a different object from our Post object itself in PostSchema
+)
+
+module.exports = mongoose.model("Post", PostSchema); //first argument is the name of the model, second argument is the schema
